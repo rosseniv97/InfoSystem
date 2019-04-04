1c90c7ec48b8926e9b66897d476835eca57d4d1f
rosseniv97 <rosseniv97@gmail.com>
2019-04-05 00:41:31 +0300

    ListBox added with greeting functionality

---
+++ Expenselt/ExpenseltHome.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Expenselt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseltHome.xaml
+    /// </summary>
+    public partial class ExpenseltHome : Page
+    {
+        public ExpenseltHome()
+        {
+            InitializeComponent();
+            ListBoxItem james = new ListBoxItem();
+            james.Content = "James";
+            peopleListBox.Items.Add(james);
+
+            ListBoxItem bobi = new ListBoxItem();
+            bobi.Content = "Bobi";
+            peopleListBox.Items.Add(bobi);
+
+            peopleListBox.SelectedItem = james;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String greetingMsg;
+            greetingMsg = (peopleListBox.SelectedItem as ListBoxItem).Content.ToString();
+            MessageBox.Show("Hi, " + greetingMsg);
+
+        }
+    }
+}
